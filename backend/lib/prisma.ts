8acb50ef9c9cf2e06333f61ed54659c85f04bb53
kenazalf <alfizza.kenaz@mail.utoronto.ca>
2026-02-19 00:24:37 -0500

    configure prisma

---
+++ backend/lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const createPrismaClient = () => {
+  const adapter = new PrismaPg({
+    connectionString: Deno.env.get("DATABASE_URL")!,
+  });
+
+  return new PrismaClient({
+    adapter,
+    log:
+      Deno.env.get("DENO_ENV") === "production"
+        ? ["error"]
+        : ["query", "error", "warn"],
+  });
+};
+
+export const prisma = globalThis.prisma ?? createPrismaClient();
+
+if (Deno.env.get("DENO_ENV") !== "production") {
+  globalThis.prisma = prisma;
+}
