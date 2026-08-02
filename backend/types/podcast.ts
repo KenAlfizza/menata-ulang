e9f13077ad91fe7bffaa35bc0a5860179b3e4383
kenazalf <alfizza.kenaz@mail.utoronto.ca>
2026-08-01 21:08:30 -0400

    hostServices and podcastRecord types

---
+++ backend/types/podcast.ts
@@ -0,0 +1,26 @@
+/**
+ * The podcast record data type
+ */
+export type PodcastRecord = {
+    id: string,
+    slug: string,
+    
+    title: string,
+    description: string,
+    transcript: string,
+    duration: number,
+    
+    audioUrl: string,
+    imageUrl: string,
+
+    createdAt: Date,
+    updatedAt: Date | null,
+    
+    published: boolean,
+    publishedAt: Date | null,
+
+    hostId: number,
+    threadId: string,
+
+    heartsCount: number,
+}
---
+++ backend/types/services/host.ts
@@ -0,0 +1,29 @@
+/**
+ * Host services data type
+ * Shared data type between API and Author Services
+ */
+
+import { PodcastRecord } from "../podcast.ts";
+
+// Services response data type
+export type HostServiceResult<PodcastRecord> =
+| { success: true; data: PodcastRecord }
+| { success: false; 
+    error: 
+        'SLUG_TAKEN' | 
+        'NOT_FOUND' |
+        'UNAUTHORIZED' |
+        'BAD_REQUEST' |
+        'INTERNAL_ERROR'
+  }
+
+export interface CreatePodcastData extends Pick<
+  PodcastRecord,
+  'slug' |
+  'title' |
+  'description' |
+  'transcript'
+> {
+    audio?: File,
+    image?: File,
+}
